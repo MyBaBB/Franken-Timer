c453d6c1107de17211eae5e66b5f0a49153413d1
Brett Baker <GitBaBB@MyBaBB.com>
2024-05-28 10:08:16 -0700

    new Frankentimer for Fathers day.  needs better but put it out and work on it more,  need to get the timer out .

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 
 const Timer = () => {
-  var countDownDate = new Date("May 27, 2024 00:00:00 PST").getTime();
+  var countDownDate = new Date("June 23, 2024 00:00:00 PST").getTime();
 
   // Update the count down every 1 second
   var x = setInterval(function () {
@@ -31,7 +31,7 @@ const Timer = () => {
     if (distance < 0) {
       clearInterval(x);
       document.getElementById("demo").innerHTML =
-        "<span style='color:rgba(235, 156, 10, 1); text-shadow: .5px .5px 0 #000;  font-size: 2.75rem;'><b>..American Soldiers!!</b></span>";
+        "<span style='color:rgba(235, 156, 10, 1); text-shadow: .5px .5px 0 #000;  font-size: 2.75rem;'><b>..Old Man!!</b></span>";
     }
   }, 1000);
 };
