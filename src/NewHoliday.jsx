291d5074b9476f44ac1bb5189d91bf523c8cbb65
Brett Baker <GitBaBB@MyBaBB.com>
2024-07-06 05:59:18 -0700

    worked on putting the top paragraph in its own wrapper along with the clock for easier maintenance

--- src/NewHoliday.jsx
+++ src/NewHoliday.jsx
@@ -4,8 +4,8 @@ import { FaHammer } from "react-icons/fa6";
 const NewHoliday = () => {
   return (
     <div className="fixed top-0 left-0 w-screen">
-    <div className="relative flex xxxs:justify-center sm:justify-between border-2 border-orange-500 ">
-      <div className=" w-fit sm:ml-4   border-2 border-purple-500 "> 
+    <div className="relative flex xxxs:justify-center sm:justify-between   ">
+      <div className=" w-fit sm:ml-4   "> 
       <a href="https://Mybabb.com"  className="  topParagraphWrapper   
               font-Kingthings_Petrock" >
         
@@ -22,11 +22,11 @@ const NewHoliday = () => {
 
       <div className="demoWrapper"> 
       <a href="https://Mybabb.com">
-                <div className="border-2 border-green-500 relative min-w-[200px] md:min-w-[320px] flex  ">
+                <div className="  relative min-w-[200px] md:min-w-[320px] flex  ">
                 <p
                   id="demo"
                   className="m-auto relative flex    w-[110%] text-lg sm:text-lg md:text-2xl lg:text-4xl
-                   border-2 border-red-500"
+                   "
                 ></p>
                 </div>
       </a> 
