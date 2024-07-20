6eef6f048773b6b572be5bda6c4927cfb2e6a781
Brett Baker <GitBaBB@MyBaBB.com>
2024-07-20 04:09:00 -0700

    fixed title back

--- src/NewHoliday.jsx
+++ src/NewHoliday.jsx
@@ -1,36 +1,41 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import { RxMoon } from "react-icons/rx";
+import { FaHammer } from "react-icons/fa6";
 const NewHoliday = () => {
   return (
-    <div className="fixed mt-4 w-screen">
-      <div className="fixed flex w-full xxxs:justify-center sm:justify-between   ">
-        <div className="demoWrapper w-full   ">
-          <a href="https://Mybabb.com ">
-            <div id="demo" className="ml-8 sm:text-xl lg:text-3xl"></div>
-          </a>
-        </div>
-
-        <div className="relative flex w-fit sm:ml-4   ">
-          <a
-            href="https://Mybabb.com"
-            className="topParagraphWrapper   
-               font-Aclonica-Regular"
-          >
-            <p
-              className="topParagraph   whitespace-nowrap 
-               text-3xl sm:text-4xl md:text-5xl lg:text-6xl   "
-            >
-              <span className="mx-2 inline-block text-blue-200">
-                <RxMoon size={39} />
-              </span>
-              <span className="inline-block text-blue-200">
-                &nbsp;Moon&nbsp;Day&nbsp;
-              </span>
-            </p>
-          </a>
-        </div>
+    <div className="fixed top-0 left-0 w-screen">
+    <div className="relative flex xxxs:justify-center sm:justify-between   ">
+      <div className=" w-fit sm:ml-4   "> 
+      <a href="https://Mybabb.com"  className="  topParagraphWrapper   
+              font-Kingthings_Petrock" >
+        
+          <p className="topParagraph   whitespace-nowrap 
+               text-3xl sm:text-4xl md:text-5xl lg:text-6xl   ">
+          <span className='text-white inline-block'  >Labor&nbsp;Day&nbsp;</span>
+          <span className='text-amber-900 inline-block mx-2' ><FaHammer size={29} /></span>
+           </p> 
+          
+          
+       
+      </a>
       </div>
+
+      <div className="demoWrapper"> 
+      <a href="https://Mybabb.com">
+                <div className="  relative    min-w-[225px] lg:min-w-[335px] flex  ">
+                <p
+                  id="demo"
+                  className="m-auto relative flex ml-8 md:ml-0  justify-start     w-full text-lg sm:text-lg md:text-2xl lg:text-4xl
+                   "
+                ></p>
+                </div>
+      </a> 
+      </div>
+      </div>
+
+
+
+
     </div>
   );
 };
