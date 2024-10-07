3a7b302c9a158b8fbbd81287de40f66998560419
Brett Baker <GitBaBB@MyBaBB.com>
2024-10-07 07:16:57 -0700

    added eyeballs to test on gh page to see if it compiles,  I might have to put it on render and update it

--- src/PregnantLadyFolder/PregnantLadyRight.jsx
+++ src/PregnantLadyFolder/PregnantLadyRight.jsx
@@ -1,16 +1,11 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-
-import PregnantLadyLeft from "./PregnantLady.svg";
+import EyeBall from "../EyeBallFolder/EyeBall";
+// import PregnantLadyLeft from "./PregnantLady.svg";
 const PregnantLadyLeftComponent = () => {
   return (
     <div className="scale-x-[-1] ">
-      <img
-        src={PregnantLadyLeft}
-        alt="Pregnant Lady Pointing Left"
-        width={110}
-        height={110}
-      />
+     < EyeBall />
     </div>
   );
 };
