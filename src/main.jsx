f31351f1b99ee26000263f58203ffd981b5688fd
Brett Baker <GitBaBB@MyBaBB.com>
2025-03-30 11:57:09 -0700

    repair

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,14 @@ import "./index.css";
 import Cube from "./CubeFolder/Cube.jsx";
 import Timer from "./Timer.jsx";
 import ThumbsUp from "./BottomComponentsFolder/ThumbsUp.jsx";
-// import NewHoliday from "./NewHoliday.jsx";
-import Easter from "./PagesFolder/EasterPage/EasterComponents/Easter.jsx";
+import NewHoliday from "./NewHoliday.jsx";
 import BottomComponents from "./BottomComponentsFolder/BottomComponents.jsx";
  
 // import AquariumDataTip from "./AquariumFolder/AquariumDataTip.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <NewHoliday /> */}
+    <NewHoliday />
 
-    <Easter />
     <Timer />
    
     <Cube />
