2b77da604e3c0bbd24ab90dad7d3acbcd1f64c2c
Brett Baker <GitBaBB@MyBaBB.com>
2025-03-30 13:10:20 -0700

    got it converted to all Easter files need to make an easter page now

---
+++ src/Pages/EasterPage.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+import EasterCube from "../Components/CubeFolder/EasterCubeFolder/EasterCube.jsx";
+import EasterTimer from "../Components/TopComponents/TimerComponents/EasterTimer/EasterTimer.jsx";
+import EasterThumbsUp from "../Components/BottomComponentsFolder/EasterBottomComponents/EasterThumbsUp.jsx";
+import EasterHoliday from "../Components/TopComponents/HolidayFolder/EasterHoliday/EasterHoliday.jsx";
+import EasterBottomComponents from "../Components/BottomComponentsFolder/EasterBottomComponents/EasterBottomComponents.jsx";
+
+// import AquariumDataTip from "./AquariumFolder/AquariumDataTip.jsx";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <EasterHoliday />
+
+    <EasterTimer />
+
+    <EasterCube />
+
+    <EasterThumbsUp />
+
+    <EasterBottomComponents />
+  </React.StrictMode>,
+);
