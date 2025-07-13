3f39e5d22d55930c1dceabf53004bafc9a03e734
Brett Baker <GitBaBB@MyBaBB.com>
2025-07-13 12:36:04 -0700

    Chiili dog day,  can work on faces in spare time

--- src/CubeFolder/Cube.jsx
+++ src/CubeFolder/Cube.jsx
@@ -3,10 +3,10 @@ import React from "react";
 
 import WolfWhistle from "../assets/wolf-whistle.wav";
 
-import One from "../assets/01Frank300.jpg"; 
+import One from "../assets/01Frank300-2.jpg"; 
 import Two from "../assets/02Frank300.jpg"; 
-import Three from "../assets/03Frank300.jpg"; 
-import Four from "../assets/04Frank300.jpg"; 
+import Three from "../assets/03Frank300-3.jpg"; 
+import Four from "../assets/04Frank300-4.jpg"; 
 import Top from "../assets/05TopFrank300.jpg";
 
  
