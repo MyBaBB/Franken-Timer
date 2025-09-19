485452cdd990ab3f0743aeb0ab4b1cfbc1cfd610
Brett Baker <GitBaBB@MyBaBB.com>
2025-09-18 23:51:43 -0700

    image work - boobies

--- src/Cube.jsx
+++ src/Cube.jsx
@@ -6,7 +6,7 @@ import WolfeWhistle from "./assets/wolf-whistle.wav";
 import Booby from "./assets/0-300pxCanoeRide2.jpg";
 import FrankenDrink from "./assets/2-300pxFrankenDrink4.jpg";
  
-import FrankenColumbus from "./assets/1-300px1FrankenColumbus2.jpg"; 
+import FrankenColumbus from "./assets/1-300px1FrankenColumbus4.jpg"; 
 import FrankenBride from "./assets/3-300pxFrankenGirlyer.jpg"
 import Top from "./assets/Choctaw300.jpg"
 import "./Cube.css";
