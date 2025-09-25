58cb31877c3272ee7d016744896b034d55cc77f4
Brett Baker <GitBaBB@MyBaBB.com>
2025-09-25 04:57:46 -0700

    made the candle bigger for clear image results

--- src/assets/Components/WallSconce.jsx
+++ src/assets/Components/WallSconce.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 import "./WallSconce.css";
-import Flame1 from "./Flame1.png";
-import Flame2 from "./Flame2.png";
-import Flame3 from "./Flame3.png";
+import Flame1 from "./WallSconceImages/WallSconce1.png";
+import Flame2 from "./WallSconceImages/WallSconce2.png";
+import Flame3 from "./WallSconceImages/WallSconce3.png";
+import Flame4 from "./WallSconceImages/WallSconce4.png";
 
-const flameFrames = [Flame1, Flame2, Flame3];
+const flameFrames = [Flame1, Flame2, Flame3, Flame4];
 
 const WallSconce = () => {
   const [currentFrame, setCurrentFrame] = useState(0);
@@ -19,13 +20,14 @@ const WallSconce = () => {
 
   return (
     <div
-      className="wall-sconce m-auto hidden
-          h-fit w-fit border-2 border-red-500 md:block"
+      className="wall-sconce m-auto hidden 
+          h-auto w-fit border-2 border-red-500 md:block"
     >
       <img
         src={flameFrames[currentFrame]}
         alt={`Flame frame ${currentFrame + 1}`}
-        className="flame"
+        loading="lazy"
+        className="flame "
       />
     </div>
   );
