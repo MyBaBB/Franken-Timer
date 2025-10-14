5d6a6ab929451ca22beec1d3b8de6a2d1541120f
Brett Baker <GitBaBB@MyBaBB.com>
2025-10-14 04:20:13 -0700

    to test

--- src/NewHoliday.jsx
+++ src/NewHoliday.jsx
@@ -4,7 +4,7 @@ import WallSconce from "./assets/Components/WallSconce";
 import { IoHourglassOutline } from "react-icons/io5";
 import WolfWhistle from "./assets/PutCandleBack.mp3";
 import WolfWhistle2 from "./assets/LookDoctor.mp3";
-
+import "./NewHoliday.css";
 function PlayWolfWhistle() {
   const DrumSolo = new Audio(WolfWhistle);
   DrumSolo.play();
@@ -17,7 +17,7 @@ function PlayWolfWhistle2() {
 
 const NewHoliday = () => {
   return (
-    <div className="fixed left-0 top-0 w-screen">
+    <div className="fixed left-0 top-0 w-screen ">
       <div className="relative flex xxxs:justify-center sm:justify-between   ">
         <div className=" w-fit sm:ml-4   ">
           <a
@@ -33,11 +33,13 @@ const NewHoliday = () => {
                text-3xl sm:text-4xl md:text-5xl lg:text-6xl   "
             > 
               &nbsp;
-              <span className="mt-2  inline-block text-[#fcb54d]  ">
+              <span className="mt-2 mb-5 relative  inline-block text-[#fcb54d]  ">
                 Halloween
               </span>
             </p>
-            <p className="wallSconceWrapper relative  pt-2 hidden sm:block bp500:block "> <WallSconce /></p>
+            <p className="wallSconceWrapper relative  pt-2   bp500:block ">
+               <WallSconce />
+            </p>
           </a>
         </div>
 
@@ -48,7 +50,7 @@ const NewHoliday = () => {
             rel="noopener noreferrer"
             onClick={PlayWolfWhistle2}
           >
-            <div className="mt-2 relative flex min-w-[225px] lg:min-w-[335px]  ">
+            <div className="mt-4 relative flex min-w-[225px] lg:min-w-[335px]  ">
               <p
                 id="demo"
                 className="relative m-auto ml-8 flex w-full justify-start text-lg sm:text-lg md:ml-0 md:text-2xl lg:text-4xl
@@ -58,9 +60,9 @@ const NewHoliday = () => {
                 <IoHourglassOutline color="lightgray" />
               </span>
             </div>
-            <p className="wallSconceWrapper2  bp500:pt-[.7rem] ">
-  <WallSconce />
-</p>
+            <p className="wallSconceWrapper2  m-auto mt-4 ">
+                <WallSconce />
+             </p>
 
           </a>
         </div>
