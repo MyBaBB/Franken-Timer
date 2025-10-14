715d25f7ab19f77b86e97d07e648f94609b4c37e
Brett Baker <GitBaBB@MyBaBB.com>
2025-10-14 11:15:15 -0700

    test run , added meta data keywords

--- src/assets/Components/WallSconce.jsx
+++ src/assets/Components/WallSconce.jsx
@@ -1,16 +1,16 @@
 import { useEffect, useState } from "react";
 import "./WallSconce.css";
-// import Flame1 from "./WallSconceImages/WallSconce1.png";
-// import Flame2 from "./WallSconceImages/WallSconce2.png";
-// import Flame3 from "./WallSconceImages/WallSconce3.png";
-// import Flame4 from "./WallSconceImages/WallSconce4.png";
-
-import FlameStone1 from "./WallSconceImages/Flame-Stone-5.jpg";
-import FlameStone2 from "./WallSconceImages/Flame-Stone-4.jpg";
-import FlameStone3 from "./WallSconceImages/Flame-Stone-3.jpg";
-import FlameStone4 from "./WallSconceImages/Flame-Stone-2.jpg";
-import FlameStone5 from "./WallSconceImages/Flame-Stone-1.jpg";
-import FlameStone6 from "./WallSconceImages/Flame-Stone-3.jpg";
+import Flame1 from "./WallSconceImages/WallSconce1.png";
+import Flame2 from "./WallSconceImages/WallSconce2.png";
+import Flame3 from "./WallSconceImages/WallSconce3.png";
+import Flame4 from "./WallSconceImages/WallSconce4.png";
+ 
+// import FlameStone2 from "./WallSconceImages/Flame-Stone-4.jpg";
+// import FlameStone3 from "./WallSconceImages/Flame-Stone-3.jpg";
+// import FlameStone4 from "./WallSconceImages/Flame-Stone-2.jpg";
+// import FlameStone5 from "./WallSconceImages/Flame-Stone-1.jpg";
+// import FlameStone6 from "./WallSconceImages/Flame-Stone-3.jpg";
+// 
 
 
 
@@ -23,7 +23,7 @@ import FlameStone6 from "./WallSconceImages/Flame-Stone-3.jpg";
 
  
 
-const flameFrames = [FlameStone1, FlameStone2, FlameStone3, FlameStone4, FlameStone5, FlameStone6];
+const flameFrames = [Flame1, Flame2, Flame3, Flame4];
 
 const WallSconce = () => {
   const [currentFrame, setCurrentFrame] = useState(0);
