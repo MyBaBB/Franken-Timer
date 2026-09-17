a90c485cb7cc0b7c24e38f7319443acf079b9b4c
Brett Baker <gitbabb@mybabb.com>
2026-09-17 08:14:00 -0700

    test color

--- src/NewHoliday.jsx
+++ src/NewHoliday.jsx
@@ -34,7 +34,7 @@ const NewHoliday = () => {
                            "
             >
              
-              <div className="relative pl-[1rem]  xs:pr-4 md:pr-2 lg:pl-5 m-auto text-center  block text-slate-300  ">
+              <div className="relative pl-[1rem]  xs:pr-4 md:pr-2 lg:pl-5 m-auto text-center  block ">
                 <u className="decoration-slate-500">
                     <span className="inline-block">
                       < GiBullyMinion />
